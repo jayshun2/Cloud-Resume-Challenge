505797bf8347c8bad6f791d0d145c0cc6d591f88
Jay French <jfrench@srhc.net>
2025-08-11 08:45:51 -0500

    removed lambda arn traces

--- site/assets/js/counter.js
+++ site/assets/js/counter.js
@@ -1,7 +1,7 @@
 const counter = document.querySelector(".visits");
 
 async function updateCounter() {
-    let response = await fetch("https://h7fsqmvotxduazvf4ghmbka6y40igcjx.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("#");
     let data = await response.json();
     counter.innerHTML = `${data.counter}`; // Assuming data contains a "counter" key
 }
